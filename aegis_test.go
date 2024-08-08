4a5638ddc0750e0df94bd900ac8be087cd34701c
Jamel Lassoued | ジャメル <41418470+Sardonyx001@users.noreply.github.com>
2024-08-08 10:13:20 +0900

    test: :sparkles: update tests

--- aegis_test.go
+++ aegis_test.go
@@ -1,8 +1,11 @@
 package aegis
 
 import (
+	"bytes"
 	"crypto/rand"
 	"fmt"
+	"io"
+	"log"
 	"os"
 	"testing"
 )
@@ -29,21 +32,21 @@ func createTempFile(size int64) (*os.File, error) {
 	return tmpFile, nil
 }
 
-func BenchmarkEncrypt(b *testing.B) {
+func BenchmarkEncryptDecrypt(b *testing.B) {
 	sizes := []int64{1024, 1024 * 1024, 10 * 1024 * 1024, 100 * 1024 * 1024, 1024 * 1024 * 1024}
-	password := "benchmarkpassword"
-
+	password := "testpassword"
 	for _, size := range sizes {
+		tmpFile, err := createTempFile(size)
+		if err != nil {
+			b.Fatalf("failed to create temp file: %v", err)
+		}
+		defer os.Remove(tmpFile.Name())
+
+		outputFile := tmpFile.Name() + ".enc"
+		defer os.Remove(outputFile)
+
+		// Run encryption benchmark
 		b.Run(fmt.Sprintf("%dB", size), func(b *testing.B) {
-			tmpFile, err := createTempFile(size)
-			if err != nil {
-				b.Fatalf("failed to create temp file: %v", err)
-			}
-			defer os.Remove(tmpFile.Name())
-
-			outputFile := tmpFile.Name() + ".enc"
-			defer os.Remove(outputFile)
-
 			b.ResetTimer()
 			for i := 0; i < b.N; i++ {
 				if err := EncryptFile(tmpFile.Name(), outputFile, password); err != nil {
@@ -52,47 +55,19 @@ func BenchmarkEncrypt(b *testing.B) {
 			}
 			b.StopTimer()
 		})
-	}
-}
 
-func BenchmarkDecrypt(b *testing.B) {
-	sizes := []int64{1024, 1024 * 1024, 10 * 1024 * 1024, 100 * 1024 * 1024, 1024 * 1024 * 1024}
-	password := "benchmarkpassword"
-
-	for _, size := range sizes {
+		decFile := tmpFile.Name() + ".dec"
+		defer os.Remove(decFile)
+		// Run decryption benchmark
 		b.Run(fmt.Sprintf("%dB", size), func(b *testing.B) {
-			tmpFile, err := createTempFile(size)
-			if err != nil {
-				b.Fatalf("failed to create temp file: %v", err)
-			}
-			defer os.Remove(tmpFile.Name())
-
 			encFile := tmpFile.Name() + ".enc"
-			if err := EncryptFile(tmpFile.Name(), encFile, password); err != nil {
-				b.Fatalf("encryption failed: %v", err)
-			}
-			defer os.Remove(encFile)
-
-			decFile := tmpFile.Name() + ".dec"
-			defer os.Remove(decFile)
-
 			b.ResetTimer()
 			for i := 0; i < b.N; i++ {
 				if err := DecryptFile(encFile, decFile, password); err != nil {
 					b.Fatalf("decryption failed: %v", err)
 				}
 
-				originalData, err := os.ReadFile(tmpFile.Name())
-				if err != nil {
-					b.Fatalf("failed to read original file: %v", err)
-				}
-
-				decryptedData, err := os.ReadFile(decFile)
-				if err != nil {
-					b.Fatalf("failed to read decrypted file: %v", err)
-				}
-
-				if !compareFiles(originalData, decryptedData) {
+				if !compareFiles(tmpFile.Name(), decFile) {
 					b.Fatalf("decrypted data does not match original data")
 				}
 			}
@@ -101,14 +76,52 @@ func BenchmarkDecrypt(b *testing.B) {
 	}
 }
 
-func compareFiles(a, b []byte) bool {
-	if len(a) != len(b) {
+const chunkSize = 64000
+
+func compareFiles(file1, file2 string) bool {
+	f1, err := os.Open(file1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f1i, err := f1.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f1.Close()
+
+	f2, err := os.Open(file2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f2i, err := f2.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f2.Close()
+
+	if f1i.Size() != f2i.Size() {
 		return false
 	}
-	for i := range a {
-		if a[i] != b[i] {
+
+	for {
+		b1 := make([]byte, chunkSize)
+		_, err1 := f1.Read(b1)
+
+		b2 := make([]byte, chunkSize)
+		_, err2 := f2.Read(b2)
+
+		if err1 != nil || err2 != nil {
+			if err1 == io.EOF && err2 == io.EOF {
+				return true
+			} else if err1 == io.EOF || err2 == io.EOF {
+				return false
+			} else {
+				log.Fatal(err1, err2)
+			}
+		}
+
+		if !bytes.Equal(b1, b2) {
 			return false
 		}
 	}
-	return true
 }
